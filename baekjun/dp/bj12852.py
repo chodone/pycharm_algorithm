afabfada2d6f05c8557ad2ae8b9ae653c26bdad1
Sungwook Cho <saeloun8797@gmail.com>
2022-11-01 05:29:12 +0900

    bj12852 completed

--- baekjun/dp/bj12852.py
+++ baekjun/dp/bj12852.py
@@ -1,45 +1,67 @@
+import copy
+
+
 def makeOne(k, curN):
     global minV
+    global res
+
+
     if curN == 1:
-        min(minV, k)
+
+        if minV > k:
+            minV = k
+            res = copy.deepcopy(divLst)
         return
 
-    if k > minV:
+    elif minV < k:
         return
+
 
     if curN % 3 == 0 and curN % 2 == 0:
+        divLst.append(curN//3)
+        makeOne(k+1, curN//3)
+        divLst.pop()
 
-        curN = curN // 3
-        calLst.append(curN)
-        makeOne(k+1, curN)
-        calLst.pop()
-        curN = curN * 3
+        divLst.append(curN//2)
+        makeOne(k+1, curN//2)
+        divLst.pop()
 
-        curN = curN // 2
-        calLst.append(curN)
-        makeOne(k+1, curN)
+        divLst.append(curN - 1)
+        makeOne(k+1, curN-1)
+        divLst.pop()
 
     elif curN % 3 == 0:
-        curN = curN // 3
-        calLst.append(curN)
-        makeOne(k + 1, curN)
+        divLst.append(curN//3)
+        makeOne(k+1, curN//3)
+        divLst.pop()
+
+        divLst.append(curN-1)
+        makeOne(k+1, curN-1)
+        divLst.pop()
 
     elif curN % 2 == 0:
-        curN = curN // 2
-        calLst.append(curN)
-        makeOne(k + 1, curN)
+
+        divLst.append(curN // 2)
+        makeOne(k + 1, curN // 2)
+        divLst.pop()
+
+        divLst.append(curN-1)
+        makeOne(k+1, curN-1)
+        divLst.pop()
 
     else:
-        curN -= 1
-        calLst.append(curN)
-        makeOne(k + 1, curN)
-
+        divLst.append(curN - 1)
+        makeOne(k + 1, curN - 1)
+        divLst.pop()
 
 
 N = int(input())
-calLst = []
-calLst.append(N)
 minV = 1e10
+divLst = []
+res = []
+divLst.append(N)
 
 makeOne(0, N)
+
 print(minV)
+print(*res)
